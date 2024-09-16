38c11d173c4e52ed84890a9e993267ec953591b2
Mr Exam <mrexamer@outlook.com>
2024-09-16 23:34:40 +0300

    Add files via upload

---
+++ ErClicker/ErClickerForm.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace ErClicker
+{
+    partial class ErClickerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErClickerForm));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.exambloglink = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.examchlink = new System.Windows.Forms.PictureBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ExamErClickerLeft = new System.Windows.Forms.RadioButton();
+            this.ExamErClickerRight = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.exambloglink)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.examchlink)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Black;
+            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 73);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(110, 31);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(10, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1,000 ms = 1 sec";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.exambloglink);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.examchlink);
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(435, 134);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // exambloglink
+            // 
+            this.exambloglink.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exambloglink.Image = ((System.Drawing.Image)(resources.GetObject("exambloglink.Image")));
+            this.exambloglink.Location = new System.Drawing.Point(184, 19);
+            this.exambloglink.Name = "exambloglink";
+            this.exambloglink.Size = new System.Drawing.Size(25, 22);
+            this.exambloglink.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.exambloglink.TabIndex = 28;
+            this.exambloglink.TabStop = false;
+            this.exambloglink.Click += new System.EventHandler(this.exambloglink_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button4.Location = new System.Drawing.Point(348, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 45);
+            this.button4.TabIndex = 27;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.Location = new System.Drawing.Point(348, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 45);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "About";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // examchlink
+            // 
+            this.examchlink.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.examchlink.Image = ((System.Drawing.Image)(resources.GetObject("examchlink.Image")));
+            this.examchlink.Location = new System.Drawing.Point(184, 105);
+            this.examchlink.Name = "examchlink";
+            this.examchlink.Size = new System.Drawing.Size(25, 22);
+            this.examchlink.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.examchlink.TabIndex = 29;
+            this.examchlink.TabStop = false;
+            this.examchlink.Click += new System.EventHandler(this.examchlink_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.numericUpDown1);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.ForeColor = System.Drawing.Color.White;
+            this.groupBox4.Location = new System.Drawing.Point(210, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(132, 115);
+            this.groupBox4.TabIndex = 16;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(3, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 14);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "60,000 ms = 1 min";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label2.Location = new System.Drawing.Point(49, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "P.S";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ExamErClickerLeft);
+            this.groupBox3.Controls.Add(this.ExamErClickerRight);
+            this.groupBox3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.Color.White;
+            this.groupBox3.Location = new System.Drawing.Point(6, 75);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(176, 52);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Select Click";
+            // 
+            // ExamErClickerLeft
+            // 
+            this.ExamErClickerLeft.AutoSize = true;
+            this.ExamErClickerLeft.BackColor = System.Drawing.Color.Transparent;
+            this.ExamErClickerLeft.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.ExamErClickerLeft.Location = new System.Drawing.Point(6, 20);
+            this.ExamErClickerLeft.Name = "ExamErClickerLeft";
+            this.ExamErClickerLeft.Size = new System.Drawing.Size(76, 17);
+            this.ExamErClickerLeft.TabIndex = 0;
+            this.ExamErClickerLeft.TabStop = true;
+            this.ExamErClickerLeft.Text = "Left Click";
+            this.ExamErClickerLeft.UseVisualStyleBackColor = false;
+            // 
+            // ExamErClickerRight
+            // 
+            this.ExamErClickerRight.AutoSize = true;
+            this.ExamErClickerRight.BackColor = System.Drawing.Color.Transparent;
+            this.ExamErClickerRight.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.ExamErClickerRight.Location = new System.Drawing.Point(88, 20);
+            this.ExamErClickerRight.Name = "ExamErClickerRight";
+            this.ExamErClickerRight.Size = new System.Drawing.Size(84, 17);
+            this.ExamErClickerRight.TabIndex = 1;
+            this.ExamErClickerRight.TabStop = true;
+            this.ExamErClickerRight.Text = "Right Click";
+            this.ExamErClickerRight.UseVisualStyleBackColor = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(6, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(176, 57);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "on Top";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.radioButton2.Location = new System.Drawing.Point(85, 20);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Always";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.radioButton1.Location = new System.Drawing.Point(3, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Never";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // ErClickerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(435, 134);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ErClickerForm";
+            this.Text = "Er-Clicker v1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ErClickerForm_FormClosing);
+            this.Load += new System.EventHandler(this.ErClickerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.exambloglink)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.examchlink)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox examchlink;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton ExamErClickerRight;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton ExamErClickerLeft;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.PictureBox exambloglink;
+    }
+}
+
---
+++ ErClicker/ErClickerForm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.ComponentModel;
+//using System.Media;
+
+
+namespace ErClicker
+{
+    public partial class ErClickerForm : Form
+    {
+        //DllImport for clicker event
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int examleftright, int RCM, int PRC, int PLC, int examupdown);
+        //DllImport for hotkey
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+        //some cmd
+        int cmdec;
+        bool start = true;
+        int interval;
+        public ErClickerForm()
+        {
+            InitializeComponent();
+        }
+        //Right click
+        public void Rightclick(Point RC)
+        {
+            mouse_event(0x0008, RC.X, RC.Y, 0, 0);
+            mouse_event(0x0010, RC.X, RC.Y, 0, 0);
+        }
+        //Left click
+        public void Leftclick(Point LC)
+        {
+            mouse_event(0x0002, LC.X, LC.Y, 0, 0);
+            mouse_event(0x0004, LC.X, LC.Y, 0, 0);
+        }
+        // send true/flase
+        public int sendyesorno()
+        {
+            if (ExamErClickerRight.Checked == true)
+            {
+                cmdec = 0;
+            }
+            else if (ExamErClickerLeft.Checked == true)
+            {
+                cmdec = 1;
+            }
+            else
+            {
+                cmdec = 2;
+            }
+            // just back if 0
+            return cmdec;
+
+        }
+        // do nothing
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = false;
+        }
+        //over all app
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = true;
+        }
+        //About ErClicker
+        private void button3_Click(object sender, EventArgs e)
+        {     
+            MessageBox.Show("Er-Clicker v1.0 | 2022" +
+    "\nit auto clicker for silkroad pvp" +
+    "\nUP Stats/silk/mastery so fast, You can use it for other games that's needed more then one click!" +
+    "\nFor more or update visit [ mrexamer.blogspot.com ]" +
+    "\nSupport us with a word of thanks." +
+    "\nMr Exam ♥", "About ER-Clicker", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        // exit
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            //nothing
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //nothing
+        }
+        //link I
+        private void exambloglink_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://mrexamer.blogspot.com/2023/04/ER-Clicker.html");
+        }
+        //link II
+        private void examchlink_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://youtu.be/gd6vE4yAnxI");
+        }
+        //on start + background active
+        private void ErClickerForm_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread EC = new Thread(ErClicker);
+            backgroundWorker2.RunWorkerAsync();
+            EC.Start();
+        }
+        //rest ZzZzZz CPU
+        private void ErClicker()
+        {
+            Thread.Sleep(2);
+        }
+        // timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //RC
+            if (cmdec == 0)
+            {
+                Rightclick(new Point(MousePosition.X, MousePosition.Y));
+            }
+            //LC
+            else
+            {
+                Leftclick(new Point(MousePosition.X, MousePosition.Y));
+            }
+
+        }
+        // on close
+        private void ErClickerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you wanna Exit ?", "Exit!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            Thread.Sleep(1);
+        }
+        // background "hoykey" it MButton = mouse scroll button you can change it as you want
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.MButton) < 0)
+                {
+                    if (sendyesorno() == 2)
+                    {
+                        MessageBox.Show("Choose Left/Right click then start!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        if (numericUpDown1.Value == 0)
+                        {
+                            MessageBox.Show("Set the time mate!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            interval = (int)numericUpDown1.Value;
+                            interval *= 1;
+                            timer1.Interval = interval;
+                            start = true;
+                            timer1_Tick(sender, e);
+                            if (!start)
+                            {
+                                timer1.Start();
+                            }
+                            Thread.Sleep(1);
+                        }
+                        Thread.Sleep(interval);
+                    }
+                    Thread.Sleep(1);
+                }Thread.Sleep(1);
+            }//END
+        }
+    }
+}
+
+
